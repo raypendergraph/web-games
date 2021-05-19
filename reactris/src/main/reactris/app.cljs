adc960ba772748d3cf89b2b29b7181016484c14c
Ray Pendergraph <rpendergraph@circadence.com>
2021-05-19 09:56:03 -0400

    Progress

--- reactris/src/main/reactris/app.cljs
+++ reactris/src/main/reactris/app.cljs
@@ -1,32 +1,19 @@
 (ns reactris.app
   (:require
-    [reagent.core :as r]
-    [reagent.dom :as rdom]
-    [big-bang.core :refer [big-bang!]]
-    [big-bang.events.browser :refer [which]]
-    [clojure.core.async :as async :refer [put! take! go-loop chan]])
+   [reagent.core :as r]
+   [reagent.dom :as rdom]
+   [big-bang.core :refer [big-bang!]]
+   [big-bang.events.browser :refer [which]]
+   [clojure.core.async :as async :refer [put! take! go-loop chan]])
   (:require-macros
-    [cljs.core.async.macros :refer [go]]))
+   [cljs.core.async.macros :refer [go]]))
 
 (defrecord Board [width height grid])
 (defrecord Cartesian [x y])
+(defrecord Player [position piece-idx rotation-idx profile color])
 (defrecord Rotation [shape profile])
 (defrecord Piece [key size rotations])
-(defrecord WorldState
-  [; The board
-   board
-   ; A Cartesian representing the upper-left point of the piece.
-   position
-   ; All augmented Pieces based on the config shapes.
-   pieces
-   ; The current Piece
-   piece-idx
-   ; The rotation index (0-4)
-   rotation-idx
-   ; The left, down and right hit profile for this piece in this rotation.
-   profile
-   ; The color of the current Piece.
-   color])
+(defrecord WorldState [board player])
 
 (def directions
   {37 :left
@@ -47,6 +34,100 @@
                 [1 1 1 1]
                 [0 0 0 0]]}})
 
+;;;;;;;;;;;;;;;;;;;
+;; helper functions
+;;;;;;;;;;;;;;;;;;;
+(defn serial-assoc
+  "associates values into `associative` (presumably a vector) in order starting at
+  `start-index` and extending the vector if required. also works with maps, but why
+  would you want to honeslty?"
+  [associative start-index new-values]
+  (let [indexes (range start-index
+                       (+ start-index
+                          (count new-values)))]
+    (apply assoc associative (interleave indexes new-values))))
+
+(defn first-when
+  "returns a tuple of [index value] for the first position matching `predicate` for
+  any `coll` where seqable? is true, else returns the value passed in. if the there are
+  no matches for the predicate in `coll` then nil is returned."
+  [predicate coll]
+  (if (seqable? coll)
+    (first (filter some?
+                   (map-indexed (fn [i v]
+                                  (when (predicate v) [i v]))
+                                coll)))
+    coll))
+
+(defn nth-column-vector
+  "Plucks the nth column from a matrix like structure and returns it as a column vector.
+  Additionally, passing `row-start` and `row-end` (exclusive) pulls a window."
+  ;;returns all rows
+  ([n matrix-vec]
+   (nth-column-vector n matrix-vec 0 (count matrix-vec)))
+  ;; returns a window of rows.
+  ([n matrix-vec row-start row-end]
+   (vec
+    (for [row (subvec matrix-vec row-start row-end)]
+      (nth row n)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Creation and config functions 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn cw-rotate-matrix [matrix-like]
+  "Rotates a matrix-like value such that the result is the same matrix except 0,0 is now w, 0 and w, 0 is now at
+  w, h.... etc.. Returns vector of rows (which is a vector of columns)"
+  (apply mapv (fn [& colls]
+                (apply vector (reverse colls)))
+         matrix-like))
+
+(defn internalize-tetromino-config
+  "Takes a Tetromino configuration and uses nil for blanks and true for the shape body as this is a more advantageous
+  scenario for Clojure."
+  [tet-config]
+  (vec (for [row tet-config]
+         (map
+          (fn [column]
+            (if (= column 0) nil true))
+          row))))
+
+(defn create-hit-profile
+  "Creates hit profile of the body of the shape from the bottom of the sandbox.
+  A number will represent the offset of the body of the tetromino for this rotation.
+  A nil represents there is no possible collision on this column."
+  [shape]
+  (let [; Since these are all square we just count the rows.
+        column-count (count shape)
+        ; The shape's columns as reversed lists because we care about the bottom.
+        columns (for [n (range column-count)]
+                  (rseq (nth-column-vector n shape)))
+        ; A lazy list of the index of items that are not nil. We only care about the first one.
+        indices (for [c columns]
+                  (map-indexed (fn [i item]
+                                 (if (nil? item) nil i))
+                               c))]
+    (for [i indices]
+      (first (filter (complement nil?) i)))))
+
+(defn create-rotations
+  "Reads a Tetromino configuration and provides Rotation snapshots of the four rotations: 0 (default) to 3."
+  [shape]
+  (vec (map (fn [rotated-shape]
+              (let [hit-profile (create-hit-profile rotated-shape)]
+                (Rotation. rotated-shape hit-profile)))
+            (take 4 (iterate cw-rotate-matrix shape)))))
+
+(defn create-pieces
+  "Creates precomputed Pieces from the configuration passed in."
+  [pieces-config]
+  (vec (map (fn [[key tet-config]]
+              (let [shape (internalize-tetromino-config tet-config)
+                    size (count tet-config)
+                    rotations (create-rotations shape)]
+                (Piece. key size rotations)))
+            (seq pieces-config))))
+
+(def pieces (create-pieces (:pieces config)))
 (def keydown-ch (chan))
 (def tick-ch (chan))
 (def state (r/atom nil))
@@ -73,80 +154,35 @@
 
 (defn grid []
   (let [_ (println "Almost")
-    grid @grid-cursor
+        grid @grid-cursor
         _ (println "Right here.")]
-  [:g
-   (for [[y row] (map-indexed vector grid)
-         [x color] (map-indexed vector row)
-         :when ((complement nil?) color)]
-     (cell x y color))]))
+    [:g
+     (for [[y row] (map-indexed vector grid)
+           [x color] (map-indexed vector row)
+           :when ((complement nil?) color)]
+       (cell x y color))]))
 
 (defn game []
-  ;; (let [{:keys [board position pieces piece-idx rotation-idx]} state]
-    [:svg {:viewBox "0 0 20 40" :preservexmlns= "http://www.w3.org/2000/svg"}
-     [:defs
-      [:pattern {:id "grid" :width "1" :height "1" :patternUnits "userSpaceOnUse"}
-       [:path {:d "M 10 0 L 0 0 0 10" :fill "none" :stroke "gray" :stroke-width "0.1"}]]]
+  [:svg {:viewBox "0 0 20 40" :preservexmlns "http://www.w3.org/2000/svg"}
+   [:defs
+    [:pattern {:id "grid" :width "1" :height "1" :patternUnits "userSpaceOnUse"}
+     [:path {:d "M 10 0 L 0 0 0 10" :fill "none" :stroke "gray" :stroke-width "0.1"}]]]
      ;; draw the grid and occupied cells
-     [grid] 
+   ;;[grid]
      ;; draw the piece
     ;;  (piece (get-in pieces [piece-idx :rotations rotation-idx :shape])
     ;;    position)
-     [:rect {:width "100%" :height "100%" :fill "url(#grid)"}]])
-
-;;;;;;;;;;;;;;;;;;;
-;; Helper functions
-;;;;;;;;;;;;;;;;;;;
-(defn serial-assoc
-  "Associates values into `associative` (presumably a vector) in order starting at
-  `start-index` and extending the vector if required. Also works with maps, but why
-  would you want to honeslty?"
-  [associative start-index new-values]
-  (let [indexes (range start-index
-                  (+ start-index
-                    (count new-values)))]
-    (apply assoc associative (interleave indexes new-values))))
-
-(defn first-when
-  "Returns a tuple of [index value] for the first position matching `predicate` for
-  any `coll` where seqable? is true, else returns the value passed in. If the there are
-  no matches for the predicate in `coll` then nil is returned."
-  [predicate coll]
-  (if (seqable? coll)
-    (first (filter some?
-             (map-indexed (fn [i v]
-                            (when (predicate v) [i v]))
-               coll)))
-    coll))
-
-(defn nth-column-vector
-  "Plucks the nth column from a matrix like structure and returns it as a column vector.
-  Additionally, passing `row-start` and `row-end` (exclusive) pulls a window."
-  ;;returns all rows
-  ([n matrix-vec]
-   (nth-column-vector n matrix-vec 0 (count matrix-vec)))
-
-  ;; returns a window of rows.
-  ([n matrix-vec row-start row-end]
-   (vec
-     (for [row (subvec matrix-vec row-start row-end)]
-       (nth row n)))))
+   [:rect {:width "100%" :height "100%" :fill "url(#grid)"}]])
 
 
-(defn cw-rotate-matrix [matrix-like]
-  "Rotates a matrix-like value such that the result is the same matrix except 0,0 is now w, 0 and w, 0 is now at
-  w, h.... etc.. Returns vector of rows (which is a vector of columns)"
-  (apply mapv (fn [& colls]
-                (apply vector (reverse colls)))
-    matrix-like))
 
 (defn movement-profile [rotations current]
   {:down  (:profile (nth rotations
-                      current))
+                         current))
    :left  (:profile (nth rotations
-                      (mod (- current 1) 4)))
+                         (mod (- current 1) 4)))
    :right (:profile (nth rotations
-                      (mod (+ current 1) 4)))})
+                         (mod (+ current 1) 4)))})
 
 (defn calculate-board-extents
   "Calculates the maximum extents of the piece's sandbox origin based on the current rotation
@@ -156,89 +192,43 @@
         profile (movement-profile rotations rotation-idx)]
     {:left  (- 0 (apply min (:left profile)))
      :right (+ (- (:width board)
-                 (:size piece))
-              (apply min (:right profile)))
+                  (:size piece))
+               (apply min (:right profile)))
      :down  (+ (apply min (:down profile))
-              (- (:height board)
-                (:size piece)))}))
+               (- (:height board)
+                  (:size piece)))}))
 
-(defn create-hit-profile
-  "Creates hit profile of the body of the shape from the bottom of the sandbox.
-  A number will represent the offset of the body of the tetromino for this rotation.
-  A nil represents there is no possible collision on this column."
-  [shape]
-  (let [; Since these are all square we just count the rows.
-        column-count (count shape)
-        ; The shape's columns as reversed lists because we care about the bottom.
-        columns (for [n (range column-count)]
-                  (rseq (nth-column-vector n shape)))
-        ; A lazy list of the index of items that are not nil. We only care about the first one.
-        indices (for [c columns]
-                  (map-indexed (fn [i item]
-                                 (if (nil? item) nil i))
-                    c))]
-    (for [i indices]
-      (first (filter (complement nil?) i)))))
 
-(defn create-rotations
-  "Reads a Tetromino configuration and provides Rotation snapshots of the four rotations: 0 (default) to 3."
-  [shape]
-  (vec (map (fn [rotated-shape]
-              (let [hit-profile (create-hit-profile rotated-shape)]
-                (Rotation. rotated-shape hit-profile)))
-         (take 4 (iterate cw-rotate-matrix shape)))))
 
-(defn internalize-tetromino-config
-  "Takes a Tetromino configuration and uses nil for blanks and true for the shape body as this is a more advantageous
-  scenario for Clojure."
-  [tet-config]
-  (vec (for [row tet-config]
-         (map
-           (fn [column]
-             (if (= column 0) nil true))
-           row))))
-
-(defn create-pieces
-  "Creates precomputed Pieces from the configuration passed in."
-  [pieces-config]
-  (vec (map (fn [[key tet-config]]
-              (let [shape (internalize-tetromino-config tet-config)
-                    size (count tet-config)
-                    rotations (create-rotations shape)]
-                (Piece. key size rotations)))
-         (seq pieces-config))))
 
 (defn create-grid
   "Creates a `h` rows by `w` (a [][]) items grid of `empty-value` for storing gameplay state."
   [w h empty-value v-padding]
   (let [augmented-h (+ h v-padding)]
     (apply vector
-      (take augmented-h
-        (repeat
-          (apply vector
-            (take w
-              (repeat empty-value))))))))
+           (take augmented-h
+                 (repeat
+                  (apply vector
+                         (take w
+                               (repeat empty-value))))))))
 
 (defn create-state [config]
   (let [{{board-width  :width
           board-height :height} :board} config
-        pieces (create-pieces (:pieces config))
         grid (create-grid board-width
-               board-height
-               nil
-               (:size (apply max-key :size pieces)))
+                          board-height
+                          nil
+                          (:size (apply max-key :size pieces)))
         piece-idx (rand-int (count pieces))
         current-position (Cartesian. (rand-int (- board-width
-                                                 (:size (nth pieces piece-idx))))
-                           0)
+                                                  (:size (nth pieces piece-idx))))
+                                     0)
         board (Board. board-width board-height grid)
-
         rotation-idx 0
         profile (movement-profile (:rotations (nth pieces piece-idx)) rotation-idx)
-        color nil]
-    (WorldState. board current-position pieces piece-idx rotation-idx profile color)))
-
-
+        ;; TODO color
+        player (Player. current-position piece-idx rotation-idx profile :blue)]
+    (WorldState. board player)))
 
 (defn overflow?
   [state direction]
@@ -251,22 +241,22 @@
         not-nil? (complement nil?)]
     (case direction
       :left (> 0
-              (+ (dec x)
-                (apply min
-                  (filter not-nil?
-                    left))))
-      :down (< height
-              (+ (inc y)
-                (- size
+               (+ (dec x)
                   (apply min
-                    (filter not-nil?
-                      down)))))
+                         (filter not-nil?
+                                 left))))
+      :down (< height
+               (+ (inc y)
+                  (- size
+                     (apply min
+                            (filter not-nil?
+                                    down)))))
       :right (< width
-               (+ (inc x)
-                 (- size
-                   (apply min
-                     (filter not-nil?
-                       right)))))
+                (+ (inc x)
+                   (- size
+                      (apply min
+                             (filter not-nil?
+                                     right)))))
       false)))
 
 (defn move-piece [state direction]
@@ -284,8 +274,8 @@
   [state direction]
   (let [grid (get-in state [:board :grid])
         position (:position state)
-        piece (nth (:pieces state)
-                (:piece-idx state))
+        piece (nth pieces
+                   (:piece-idx state))
         profile (get (:profile state) direction)
         ;; zip the column offsets with the x coordinate
         piece-size (:size piece)
@@ -293,21 +283,20 @@
         bottom-idx (dec (+ (:y position) (:size piece)))
         ;; zip of the  [x coordinate , coordinate of the profile in terms of the grid].
         column-defs (map vector (range (:x position) piece-size)
-                      (map (fn [y-offset]
-                             (- bottom-idx y-offset))
-                        profile))
+                         (map (fn [y-offset]
+                                (- bottom-idx y-offset))
+                              profile))
         not-nil? (complement nil?)]
     ;; If any column in front of an occupied block is present then a hit will result.
     (not-nil? (some not-nil?
-                (flatten (for [[x y-edge] column-defs]
-                           (nth-column-vector x grid y-edge (inc y-edge))))))))
-
+                    (flatten (for [[x y-edge] column-defs]
+                               (nth-column-vector x grid y-edge (inc y-edge))))))))
 
 (defn emplace-piece
   "Stores a piece on the board, in its final resting place."
   [state]
   (let [grid (get-in state [:board :grid])
-        piece (nth (:pieces state) (:piece-idx state))
+        piece (nth pieces (:piece-idx state))
         rotation (nth (:rotations piece) (:rotation-idx state))
         position (:position state)
         color (:color state)
@@ -318,7 +307,7 @@
                       (when (not-nil? cell) color)))
         ;; Extract the rows that the shape will occupy depending on the size.
         shape-rows (subvec grid (:y position) (+ (:y position)
-                                                (:size piece)))
+                                                 (:size piece)))
         ;; Put the new cells into those rows.
         new-rows (for [[cells row] (map vector new-cells shape-rows)]
                    (serial-assoc row (:x position) cells))
@@ -326,23 +315,8 @@
         new-grid (serial-assoc grid (:y position) new-rows)]
     (assoc-in state [:board :grid] new-grid)))
 
-
-;;;;;;;;;;;;;;;;;;;;;;
-;; Big Bang callbacks
-;;;;;;;;;;;;;;;;;;;;;;
-
-(defn update-state [event state]
-  state)
-
-
-(defn formatted-json [state]
-  (fn []
-    [:pre (.stringify js/JSON (clj->js state) nil 3)]))
-
 (defn create-test-state [state]
-  (let [
-        {{grid :grid} :board} state
-_ (println state)
+  (let [{{grid :grid} :board} state
         new-grid (for [[r row] (map-indexed vector grid)]
                    (if (> r 5)
                      (let [i (rand-int (count row))
@@ -351,18 +325,17 @@ _ (println state)
                      row))]
     (assoc-in state [:board :grid] new-grid)))
 
-
 (defn init []
   (reset! state (create-test-state (create-state config)))
-
+  (println "State created")
   (do
     (.addEventListener js/document
-      "keydown"
-      (fn [e]
-        (let [direction (get directions (.-which e))]
-          (when ((complement nil?) direction)
-            (go (>! keydown-ch
-                    direction))))))
+                       "keydown"
+                       (fn [e]
+                         (let [direction (get directions (.-which e))]
+                           (when ((complement nil?) direction)
+                             (go (>! keydown-ch
+                                     direction))))))
 
     (js/setInterval (fn [] (go (>! tick-ch true))) 2000)
 
@@ -376,10 +349,7 @@ _ (println state)
     ;; Convert key down event changes into potentially new state and push
     ;; that into the state atom.
     (go-loop []
-       (let [direction (<! keydown-ch)]
+      (let [direction (<! keydown-ch)]
         (r/rswap! state (move-piece @state direction))
         (recur)))
-
-    (rdom/render [game] (.getElementById js/document "game"))
-      
-      ))
+    (rdom/render [game] (.getElementById js/document "game"))))
